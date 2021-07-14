464368e2beb4108d3ae6cc4f97cca7a274494d74
Kojee <marco.filippozzi98@gmail.com>
2021-07-14 21:35:53 +0200

    Aggiungere i file di progetto.

---
+++ MarkdownBlogPagePublisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MarkdownBlogPagePublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1) INPUT: input folder, output file path, azure/local publication flag
+             *      1.1) if azure: pass storage name and container name
+             *      1.2) if local: pass output folder path for images
+             * 2) azure:
+             *      1) interactive login
+             *      2) container creation
+             *      3) upload with anonymous access enabled
+             *      4) return list of direct download links
+             * 3) local:
+             *      ...
+             * 4) markdown file creation
+             */
+        }
+    }
+}
